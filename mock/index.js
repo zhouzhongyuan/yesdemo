cc47241fc18a310b5623a3f20c27cf668916d164
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-06-08 10:01:57 +0800

    Update mock judgment data source

--- mock/index.js
+++ mock/index.js
@@ -1,8 +1,6 @@
-let __dev__ = true;
-// __dev__ = false;
 import FetchMock from 'react-native-fetch-mock';
-
-if (__dev__) {
+import appOptions from '../src/app/config';
+if (appOptions.mock) {
     global.fetch = new FetchMock(require('../__mock__')).fetch;
 }
 
