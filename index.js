5d4dbc780ca99c61d9572ec4be32e7cba7d09e85
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-06-09 14:27:26 +0800

    Add debuger for react-native

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import appOptions from './src/app/config';
+if (appOptions.debug) {
+    <script src="http://192.168.123.224:8097"></script>
+}
 import './mock/';
 import './src/entry.native';
 export default {};
